ad19c77824f2cb489a78d1c60bc7f097095e9ba8
Kamil Błaut <blaut.kamil@outlook.com>
2026-09-21 13:01:25 +0200

    Add translator tests

--- tests/test_translator.py
+++ tests/test_translator.py
@@ -120,12 +120,12 @@ def test_translate_with_not_supplied_kwargs(tmp_path):
     translator.set_locale("test")
 
     #Act
-    translation = translator.translate("hello",kwarg1="test1")
+    translation = translator.translate("hello")
 
     #Assert
     assert translation == "test {kwarg1} {kwarg2}"
 
-def test_translate_with__not_all_kwargs_supplied(tmp_path):
+def test_translate_with_not_all_kwargs_supplied(tmp_path):
     #Arrange
     locales_dir = tmp_path/"locales"
     locales_dir.mkdir()
